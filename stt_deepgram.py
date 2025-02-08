30dc3122c3bb5acd6b91bc62e85208729aa04455
akashi-m <atos4ik@gmail.com>
2025-02-08 14:54:58 +0500

    First attempt
    And it is successfully played )))

---
+++ stt_deepgram.py
@@ -0,0 +1,38 @@
+# Copyright 2024 Deepgram SDK contributors. All Rights Reserved.
+# Use of this source code is governed by a MIT license that can be found in the LICENSE file.
+# SPDX-License-Identifier: MIT
+
+import asyncio
+import os
+
+from dotenv import load_dotenv
+import logging
+from deepgram.utils import verboselogs
+
+from deepgram import (
+    DeepgramClient
+)
+
+load_dotenv()
+
+SPEAK_TEXT = {"text": "It is a very pleasant that I can speak with you finally )"}
+filename = "data/audio_output/test.mp3"
+
+
+async def main():
+    try:
+        print("Connecting to Deepgram...")
+        # Инициализация клиента
+        deepgram = DeepgramClient(os.getenv("DEEPGRAM_API_KEY"))
+
+        response = await deepgram.speak.asyncrest.v("1").save(
+            filename, SPEAK_TEXT
+        )
+        print(response.to_json(indent=4))
+
+    except Exception as e:
+        print(f"Exception: {e}")
+
+# Ваш текущий код работает нормально
+if __name__ == "__main__":
+    asyncio.run(main())
